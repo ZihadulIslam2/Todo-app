db0f349b2a2c3c85a0b77bf5f56158aa1c0830cc
zihadulIslam2 <zihadul708@gmail.com>
2025-01-25 09:19:25 +0600

    todo items

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -6,3 +6,25 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+const TodoItem = ({ item, onDelete }) => {
+    return(
+        <TouchableOpacity onPress={()=>onDelete(item.id)}>
+            <View style={styles.todoItem}>
+                <Text>{item.value}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+   todoItem: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f8f8f8',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5
+}
+})
+
+export default TodoItem;
