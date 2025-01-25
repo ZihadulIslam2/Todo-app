fca23eaff4261dc15dbd6222fdf7e9c45dad0d6e
zihadulIslam2 <zihadul708@gmail.com>
2025-01-25 10:17:13 +0600

    first commit

--- App.js
+++ App.js
@@ -1,40 +1,59 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View,FlatList, Button, TextInput,  } from 'react-native'
-import TodoItem from './components/TodoItem';
-import AddTodo from './components/AddTodo';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, FlatList } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import TodoItem from './components/TodoItem'
+import AddTodo from './components/AddTodo'
 
 export default function App() {
   const [todos, setTodos] = useState([])
 
-  const addTodoHandler = (todo)=>{
-    setTodos((setTodos)=>[ 
-      ...currentTodos,
-      { id: Math.random().toString(), value: todo}
-    ])
-  };
+  // Load todos from AsyncStorage when the component mounts
+  useEffect(() => {
+    const loadTodos = async () => {
+      const savedTodos = await AsyncStorage.getItem('todos')
+      if (savedTodos) {
+        setTodos(JSON.parse(savedTodos))
+      }
+    }
+    loadTodos()
+  }, [])
 
-  const deleteTodoHandler = (todoId)=>{
-    setTodos((currentTodos)=>{
-      return currentTodos.filter((todo)=> todo.id != todoId)
+  // Save todos to AsyncStorage whenever the todos state changes
+  useEffect(() => {
+    AsyncStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  // Function to add a new todo
+  const addTodoHandler = (todo) => {
+    setTodos((currentTodos) => [
+      ...currentTodos,
+      { id: Math.random().toString(), value: todo },
+    ])
+  }
+
+  // Function to delete a todo
+  const deleteTodoHandler = (todoId) => {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== todoId)
     })
   }
 
   return (
     <View style={styles.container}>
-     <AddTodo onAddTodo={addTodoHandler}/>
-     <FlatList 
-     data={todos}
-     renderItem={({item})=>(
-      <TodoItem item={item} onDelete={deleteTodoHandler}/>
-     )}
-     keyExtractor={(item, index)=>item.id}
-     />
+      <AddTodo onAddTodo={addTodoHandler} />
+      <FlatList
+        data={todos}
+        renderItem={({ item }) => (
+          <TodoItem item={item} onDelete={deleteTodoHandler} />
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-   padding: 50
+    padding: 50,
   },
 })
